3d296745d44a5c53187d1812a64cc2aa8533ce69
Haseeb <haziboy604@gmail.com>
2020-07-04 22:44:26 +0500

    add cors

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    return 123;
+    //    return $request->user();
 });
 Route::post('/login', 'APILoginController@login');
 Route::post('/register','APILoginController@register');
